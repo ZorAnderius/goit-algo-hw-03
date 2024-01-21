c766640d57cd32071a548d7d007d8561e8b39472
ZorAnderius <gabriel.zorander@gmail.com>
2024-01-21 14:30:37 +0000

    added copy files function (with test folder picture) and function for draw koch snowflake

--- task1.py
+++ task1.py
@@ -1,5 +1,37 @@
-def task1():
-    pass
+import argparse
+from pathlib import Path
+import shutil
+import sys
+
+# parse arguments from command line
+def parse_argv() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Copying files from directories and sorting them by type')
+    parser.add_argument('-s', '--sources', type=Path, required=True,  help='Path to source files')
+    parser.add_argument('-o', '--output',type=Path, default=Path('dict'), help='Path to output files (default is dict)')
+    return parser.parse_args()
+
+# function to copy files from directory and sort them by type
+def recursion_copying_files(source: Path, output: Path) -> None:
+    for element in source.iterdir():
+        try:
+            if element.is_dir():
+                recursion_copying_files(element, output)
+            else:
+                folder = output / element.suffixes[-1][1:]
+                folder.mkdir(parents=True, exist_ok=True)
+                shutil.copy(element, folder)
+        except Exception as e:
+            print(f'Problem with access to {element}: {e}')
+
+# main function Task1
+def task1() -> None:
+    args = parse_argv()
+    if Path(args.sources).exists():
+        recursion_copying_files(args.sources, args.output)
+    else:
+        print(f'{args.sources} path does not exist')
+        sys.exit(0)
+
 
 if __name__ == '__main__':
     task1()
--- task2.py
+++ task2.py
@@ -1,5 +1,47 @@
+import turtle
+
+def koch_snowflake(t, order, size):
+    if order == 0:
+        t.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_snowflake(t, order - 1, size / 3)
+            t.left(angle)
+
+def draw_koch_snowflake(order, size=420):
+    wind = turtle.Screen()
+    wind.bgcolor("black")
+    
+    turtle.TurtleScreen._RUNNING=True
+    t = turtle.Turtle()
+    t.color('white')
+    t.speed(0)
+    t.penup()
+    t.goto(-size / 2, size / 4)
+    t.pendown()
+    
+    for _ in range(3):
+        koch_snowflake(t, order, size)
+        t.right(120)
+        
+    wind.mainloop()
+    
 def task2():
-    pass
+    while True:
+        try:
+            input_arg = int(input('Enter the level of recursion: (close to -1))'))
+        except ValueError:
+            print('Incorrect level of recursion. Please use numbers only')
+            continue
+        if input_arg ==-1:
+            print('Good bye!')
+            break
+    
+        if input_arg < 0:
+            print('Incorrect level of recursion. Please use numbers only')
+            continue
+        
+        draw_koch_snowflake(int(input_arg))
 
 if __name__ == '__main__':
     task2()
